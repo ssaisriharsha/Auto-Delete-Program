5baed13a442e48a55f60f47111b988a894942732
programmer24680 <69705177+programmer24680@users.noreply.github.com>
2022-03-27 22:41:55 +0530

    Introducing Folder Deletion And Bug Fixes

--- Auto.py
+++ Auto.py
@@ -1,15 +1,68 @@
-import datetime
-import os
+def ending():
+    input("Press enter to exit the program.")
+    exit()
 
-file_path = input("enter the file path: ")
-delete_time = input("Type when file to be deleted(HH:MM:SS) in 24 hrs format: ")
-print("Performing the operation....")
-while True:
-    date_and_time = datetime.datetime.now()
-    time = date_and_time.strftime("%H:%M:%S")
-    if time == delete_time:
-        os.remove(file_path)
-        print("Operation Successful!")
-        break
+
+try:
+    import datetime
+    import os
+    import shutil
+    import time
+
+    print("Select what you want to do:")
+    time.sleep(0.25)
+    print("1. Delete a file")
+    print("2. Delete a folder")
+    time.sleep(0.5)
+    selection = input("Enter your choice: ")
+
+    """try:
+        int(selection)
+    except ValueError:
+        print("you have to enter either 1 or 2 only.")
+        ending()"""
+
+    if selection == "1":
+        file_path = input("enter the file path: ")
+        delete_time = input("Type when file to be deleted(HH:MM:SS) in 24 hrs format: ")
+        print("Performing the operation....")
+        while True:
+            date_and_time = datetime.datetime.now()
+            time = date_and_time.strftime("%H:%M:%S")
+            if time == delete_time:
+                try:
+                    os.unlink(file_path)
+                    print("Operation Successful!")
+                except FileNotFoundError:
+                    print("The file path that has been entered is invalid. Please enter a valid file path and try "
+                          "again.")
+                except PermissionError:
+                    print("You don't have sufficient rights to perform this operation.")
+                finally:
+                    break
+            else:
+                continue
+
+    elif selection == "2":
+        folder_path = input("enter the folder path: ")
+        delete_time = input("Type when file to be deleted(HH:MM:SS) in 24 hrs format: ")
+        print("Performing the operation....")
+        while True:
+            date_and_time = datetime.datetime.now()
+            time = date_and_time.strftime("%H:%M:%S")
+            if time == delete_time:
+                try:
+                    shutil.rmtree(folder_path)
+                    print("Operation Successful.")
+                except FileNotFoundError:
+                    print("The folder doesn't exist in the path provided. Please check the path once again")
+                except PermissionError:
+                    print("You don't have sufficient rights to perform this action.")
+                finally:
+                    break
     else:
-        continue
+        print("you have to enter either 1 or 2 only.")
+    ending()
+except KeyboardInterrupt:
+    print("Action was interrupted.")
+    ending()
